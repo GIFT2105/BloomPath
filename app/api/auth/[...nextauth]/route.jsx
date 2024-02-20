424b7149e5043062ef0a6a78287bf765306478ee
GIFT2105 <letuka.gift21@gmail.com>
2024-02-21 00:48:43 +0200

    route handler error fix

--- app/api/auth/[...nextauth]/route.jsx
+++ app/api/auth/[...nextauth]/route.jsx
@@ -4,45 +4,46 @@ import NextAuth from "next-auth/next";
 import CredentialsProvider from "next-auth/providers/credentials";
 import bcrypt from "bcryptjs";
 
-export const authOptions = {
-  providers: [
-    CredentialsProvider({
-      name: "credentials",
-      credentials: {},
+export default async function handler(req, res) {
+  const authOptions = {
+    providers: [
+      CredentialsProvider({
+        name: "credentials",
+        credentials: {},
 
-      async authorize(credentials) {
-        const { email, password } = credentials;
+        async authorize(credentials) {
+          const { email, password } = credentials;
 
-        try {
-          await connectMongoDB();
-          const user = await User.findOne({ email });
+          try {
+            await connectMongoDB();
+            const user = await User.findOne({ email });
 
-          if (!user) {
+            if (!user) {
+              return null;
+            }
+
+            const passwordsMatch = await bcrypt.compare(password, user.password);
+
+            if (!passwordsMatch) {
+              return null;
+            }
+
+            return user;
+          } catch (error) {
+            console.log("Error: ", error);
             return null;
           }
+        },
+      }),
+    ],
+    session: {
+      strategy: "jwt",
+    },
+    secret: process.env.NEXTAUTH_SECRET,
+    pages: {
+      signIn: "/",
+    },
+  };
 
-          const passwordsMatch = await bcrypt.compare(password, user.password);
-
-          if (!passwordsMatch) {
-            return null;
-          }
-
-          return user;
-        } catch (error) {
-          console.log("Error: ", error);
-        }
-      },
-    }),
-  ],
-  session: {
-    strategy: "jwt",
-  },
-  secret: process.env.NEXTAUTH_SECRET,
-  pages: {
-    signIn: "/",
-  },
-};
-
-const handler = NextAuth(authOptions);
-
-export { handler as GET, handler as POST };
+  await NextAuth(req, res, authOptions);
+}
