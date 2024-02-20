b79ad785f1de028c42f246e3393e936bf2fd9b14
GIFT2105 <letuka.gift21@gmail.com>
2024-02-21 00:27:03 +0200

    first commit

---
+++ components/Worldnews.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import TopHeadlines from './TopHeadlines'
+
+const Worldnews = () => {
+  return (
+    <div>
+      news page
+      <TopHeadlines />
+    </div>
+  )
+}
+
+export default Worldnews
